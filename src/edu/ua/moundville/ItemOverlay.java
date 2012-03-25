cddf8f719b1a97ced716aa88e67248bfbf0baad9
Darwin <dcwitt@crimson.ua.edu>
2012-03-25 16:27:50 -0500

    Added stub for onTap event

--- src/edu/ua/moundville/ItemOverlay.java
+++ src/edu/ua/moundville/ItemOverlay.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 
 import android.graphics.drawable.Drawable;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.MapView;
 import com.google.android.maps.OverlayItem;
 
 public class ItemOverlay extends ItemizedOverlay<OverlayItem> {
@@ -28,6 +30,24 @@ public class ItemOverlay extends ItemizedOverlay<OverlayItem> {
 	@Override
 	public int size() {
 		return items.size();
+	}
+	
+	public boolean onTap (final GeoPoint p, final MapView mapView){
+	    boolean tapped = super.onTap(p, mapView);
+	    if (tapped){            
+	        //do what you want to do when you hit an item - i.e. drawInfoBox(...)    
+	    }           
+	    else{
+	        //do what you want to do when you DONT hit an item
+	        }                   
+	    return true;
+	}
+
+	//You must have this method, even if it doesn't visibly do anything
+
+	@Override
+	protected boolean onTap(int index) {
+	    return true;
 	}
 	
 }
