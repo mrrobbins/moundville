080ed64e7165397393fb7095bf59ba52775e93ba
Darwin <dcwitt@crimson.ua.edu>
2012-04-06 16:57:00 -0500

    Update to SiteMap - NEEDS TESTING

--- src/edu/ua/moundville/SiteMap.java
+++ src/edu/ua/moundville/SiteMap.java
@@ -27,14 +27,14 @@ public class SiteMap extends PlaceMap implements DBResult {
     protected static DBHandler db = new DBHandler();
     protected ArrayList<NameValuePair> queryArgs = new ArrayList<NameValuePair>();
     protected ArrayList<ArrayList<String>> items = new ArrayList<ArrayList<String>>();
-    protected ArrayList<String> record = new ArrayList<String>();
+    
     private GeoPoint RecordLoc = new GeoPoint(0, 0);
 	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
-        Drawable mapMarker = this.getResources().getDrawable(R.drawable.pin);
+        mapMarker = this.getResources().getDrawable(R.drawable.pin);
         itemOverlay = new CustomItemizedOverlay<CustomOverlayItem>(mapMarker,mapView);
         
         items = null;
@@ -52,6 +52,8 @@ public class SiteMap extends PlaceMap implements DBResult {
 		if (items != null) {
 			
 			for (int i=0; i<items.size(); i++) {
+				ArrayList<String> record = new ArrayList<String>();
+				
 				record = items.get(i);
 				
 				RecordLoc.equals(new GeoPoint(Integer.parseInt(record.get(3)),Integer.parseInt(record.get(4))));
@@ -63,15 +65,13 @@ public class SiteMap extends PlaceMap implements DBResult {
 			mapOverlays.add(itemOverlay);
 		}
 		
-		else{
-			
-		}
+	
 	}
 	
 	private void getSites() {
 		queryArgs.add(new BasicNameValuePair("case","8"));
 		db.sendQuery(this, queryArgs);
-	
+	    
 	}
 
 	public void receiveResult(JSONArray jArray) {
@@ -86,22 +86,25 @@ public class SiteMap extends PlaceMap implements DBResult {
 			
 			for (int i=0; i<jArray.length(); i++) {
 				JSONObject obj = null;
+				
 				try {
 					obj = (JSONObject) jArray.get(i);
+					ArrayList<String> record = new ArrayList<String>();
 					
-					record.add(obj.getString("pk_Site_siteID"));
+					record.add(obj.getString("pk_Site_SiteID"));
 					record.add(obj.getString("ak_Site_SiteName"));
 					record.add(obj.getString("Site_Body2"));
 					record.add(obj.getString("Site_Latitude"));
 					record.add(obj.getString("Site_Longitude"));
 					record.add(obj.getString("Img_Image"));
+
+
+					Log.d(TAG, record.get(2).toString());
 					
 					items.add(record);
-					record = null;
 					
-
 				} catch (JSONException e) {
-					// TODO Auto-generated catch block
+					
 					e.printStackTrace();
 				}
 			}
