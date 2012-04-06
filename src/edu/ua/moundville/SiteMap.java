abfb508b55fbed9b129eb77bbc9941cfad7434b5
Darwin <dcwitt@crimson.ua.edu>
2012-04-06 15:22:59 -0500

    SiteMap update for Dynamic Data - NEEDS TESTING

--- src/edu/ua/moundville/SiteMap.java
+++ src/edu/ua/moundville/SiteMap.java
@@ -1,17 +1,34 @@
 package edu.ua.moundville;
 
 
+import java.util.ArrayList;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.R.integer;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.maps.GeoPoint;
 
 import com.readystatesoftware.mapviewballoons.*;
 
-public class SiteMap extends PlaceMap {
+import edu.ua.moundville.DBHandler.DBResult;
+
+public class SiteMap extends PlaceMap implements DBResult {
 	private final static GeoPoint MOUNDVILLE_SITES = new GeoPoint(33005263, -87631438);
 	CustomItemizedOverlay<CustomOverlayItem> itemOverlay;
     private static Drawable mapMarker;
+    protected static DBHandler db = new DBHandler();
+    protected ArrayList<NameValuePair> queryArgs = new ArrayList<NameValuePair>();
+    protected ArrayList<ArrayList<String>> items = new ArrayList<ArrayList<String>>();
+    protected ArrayList<String> record = new ArrayList<String>();
+    private GeoPoint RecordLoc = new GeoPoint(0, 0);
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -19,29 +36,77 @@ public class SiteMap extends PlaceMap {
 	    super.onCreate(savedInstanceState);
         Drawable mapMarker = this.getResources().getDrawable(R.drawable.pin);
         itemOverlay = new CustomItemizedOverlay<CustomOverlayItem>(mapMarker,mapView);
+        
+        items = null;
        
+        getSites();
         
-        itemOverlay.addOverlay(new CustomOverlayItem(MOUNDVILLE_LOCATION_CENTER, "Moundville", "The Geographical Center of the Park","", "0"));
-        itemOverlay.addOverlay(new CustomOverlayItem(MOUNDVILLE_MOUND_A, "Mound A","The second largest mound in the park","","1"));
-        itemOverlay.addOverlay(new CustomOverlayItem(MOUNDVILLE_MUSEUM, "MOundville Museum","A collection of pottery awaits.","","2"));
-        
-        mapOverlays.add(itemOverlay);
         
         populateMap();
 	}
 	
 	protected void populateMap() {
-		int items = 0;
 		
 		
 		/* check or app will crash */
-		if (items != 0) {
+		if (items != null) {
+			
+			for (int i=0; i<items.size(); i++) {
+				record = items.get(i);
+				
+				RecordLoc.equals(new GeoPoint(Integer.parseInt(record.get(3)),Integer.parseInt(record.get(4))));
+					
+				itemOverlay.addOverlay(new CustomOverlayItem( RecordLoc , record.get(1), record.get(2),record.get(5), record.get(0)));
+				
+			}
+			
 			mapOverlays.add(itemOverlay);
+		}
+		
+		else{
+			
 		}
 	}
 	
 	private void getSites() {
+		queryArgs.add(new BasicNameValuePair("case","8"));
+		db.sendQuery(this, queryArgs);
 	
+	}
+
+	public void receiveResult(JSONArray jArray) {
+		
+		
+		if (jArray == null) {
+			items = null;
+		} else {
+
+			Log.d(TAG, jArray.toString());
+
+			
+			for (int i=0; i<jArray.length(); i++) {
+				JSONObject obj = null;
+				try {
+					obj = (JSONObject) jArray.get(i);
+					
+					record.add(obj.getString("pk_Site_siteID"));
+					record.add(obj.getString("ak_Site_SiteName"));
+					record.add(obj.getString("Site_Body2"));
+					record.add(obj.getString("Site_Latitude"));
+					record.add(obj.getString("Site_Longitude"));
+					record.add(obj.getString("Img_Image"));
+					
+					items.add(record);
+					record = null;
+					
+
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
 	}
 }
         /* adding overlays */
